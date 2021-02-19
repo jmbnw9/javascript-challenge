0213650c7759d125ada35a10a849cd21756c02c3
Janelle <jmbnw9@gmail.com>
2021-02-18 19:44:57 -0600

    updating

--- UFO-Level-1/static/js/app.js
+++ UFO-Level-1/static/js/app.js
@@ -1,4 +1,14 @@
 // from data.js
 var tableData = data;
 
-// YOUR CODE HERE!
+// Get a reference to the table body
+var tbody = d3.select("tbody");
+
+// Use d3 to append one table row `tr` for each ufo data object
+tableData.forEach((ufodata) => {
+  var row = tbody.append("tr");
+  Object.entries(ufodata).forEach(([key, value]) => {
+    var cell = row.append("td");
+    cell.text(value);
+  });
+});
